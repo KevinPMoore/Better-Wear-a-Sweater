385706871513f2479b57c3e3cadc9def59567ae7
Kevin Moore <moore.kevin.p@gmail.com>
2021-01-04 12:25:10 -0500

    set up basic info for the Report component and updated ReadMe notes

--- src/Components/Report/Report.js
+++ src/Components/Report/Report.js
@@ -4,17 +4,24 @@ function Report(props) {
 
     return(
         <div
-            className='Report'
+            className={['Report', props.weatherReport.current.weather[0].main].join(' ')}
         >
             <p
-                className='CurrentTemp'
+                className='Current'
             >
-                Its {props.weatherReport.current.temp}F outside right now.
+                Its {props.weatherReport.current.temp}F outside right now. <br/>
+                But it feels like {props.weatherReport.current.feels_like}F.
             </p>
             <p
-                className='FeelsLike'
+                className='Daily'
             >
-                But it feels like {props.weatherReport.current.feels_like}F.
+                Today's weather will be {props.weatherReport.daily[0].weather[0].main} <br/>
+                High: {props.weatherReport.daily[0].temp.max} <br/>
+                Low:  {props.weatherReport.daily[0].temp.min} <br/>
+                Morning: {props.weatherReport.daily[0].temp.morn} <br/>
+                Midday: {props.weatherReport.daily[0].temp.day} <br/>
+                Evening: {props.weatherReport.daily[0].temp.eve} <br/>
+                Night: {props.weatherReport.daily[0].temp.night} <br/>
             </p>
         </div>
     );
